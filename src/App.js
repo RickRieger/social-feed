1b5c7373c412b3387858f16a91a8c81e92c5ab1f
Rick Rieger <rickrieger36251@gmail.com>
2022-04-04 15:57:43 -0400

    built navbar + form component to mimic the example with out functionality using bootstrap react + css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-
+import NavBar from "./Components/NavBar/NavBar";
+import Post from "./Components/Post/Post";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-        <h1>Hello World</h1>
+      <NavBar/>
+      <Post/>
     </div>
   );
 }
