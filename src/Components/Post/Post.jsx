9292a36e71f56a3a9e2a1ea41b4d8bf33bc31f24
Rick Rieger <rickrieger36251@gmail.com>
2022-04-05 11:52:57 -0400

    installed momment.js and displayed date in post.js for each post

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import moment from 'moment';
 import "./Post.css"
 
 function Post({post}) {
@@ -29,8 +30,9 @@ console.log(post)
   return (
     <>
 
-       <div className="name post-item">{post.name}</div>
+       <div className="name post-item" style={{fontWeight:'bolder', fontSize:'1rem'}}>{post.name}</div>
         <div className="post post-item">{post.comment}</div>
+        <div className="date">Posted on: {moment(post.date).format("MMM Do YYYY")}</div>
       
         <div className="thumbs post-item">        
             <span className="glyphicon glyphicon-thumbs-up"  onClick={()=>handleThumbsOnClick('thumbs-up')}  style={{color:thumbsUp?'#32CD32':'grey'}}></span>            
